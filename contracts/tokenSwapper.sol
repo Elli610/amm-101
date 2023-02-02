5a8ffd56a472622bc929bd011b06a32046f47d22
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-02-02 08:27:17 +0100

    ok

--- contracts/tokenSwapper.sol
+++ contracts/tokenSwapper.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.6.0;
 import "./IUniswapV2Router02.sol";
 
 contract TokenSwap {
+    uint256 _amountOutMin = 100000;
 
     // address of the token to be swapped
     address public myTokenAddress = 0x08BF78D9b3Dff98A2eCb6E6a85e5d6e6694CE2bb;
@@ -20,7 +21,7 @@ contract TokenSwap {
         address[] memory path = new address[](2);
         path[0] = tokenIn;
         path[1] = tokenOut;
-        uint256 amountOutMin = 1;
+        uint256 amountOutMin = _amountOutMin;
         router.swapExactTokensForETH(amountIn, amountOutMin, path, msg.sender, block.timestamp);
     }
 
@@ -30,5 +31,9 @@ contract TokenSwap {
         address tokenOut = wethAddress;
         swap(tokenIn, amountIn, tokenOut);
     }
-    
+
+    function editAmountOutMin(uint256 amountOutMin) public {
+        _amountOutMin = amountOutMin;
+    }
+    // deployed 0x32835e12E2A80D4137C2E3e71AEF4f24CA1229Ef
 }
